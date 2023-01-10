aa3e809862a67314637126bf167f4d90f49eb122
Yuya Inoue <yuyatuta@icloud.com>
2023-01-10 16:52:17 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from extractLips import *
+import os
+import env
+
+def main():
+    root = env.ROOT
+    save_root = env.SAVE_ROOT
+    if save_root is None:
+        save_root = root
+    err_log = env.ERR_LOG
+    path_file = env.PATH_FILE
+    not_use = env.NOT_USE
+    kind1 = env.KIND1
+    kind2 = env.KIND2
+    
+
+main()
