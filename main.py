88ed8750d24e2e0bff56aed56dbed32fefa0c912
Inoue416 <57441203+Inoue416@users.noreply.github.com>
2023-01-11 00:21:46 +0000

    edit main.py save_path

--- main.py
+++ main.py
@@ -16,27 +16,27 @@ def main():
         not_use = []
     for k1 in env.KIND1:
         path = os.path.join(root, k1, env.KIND2)
-        save_path = os.path.join(save_root, k1, env.SAVE_TYPE)
-        make_folder(save_path)
+        save_path1 = os.path.join(save_root, k1, env.SAVE_TYPE)
+        make_folder(save_path1)
         for k3 in os.listdir(path):
             if (k3 in not_use):
                 continue
             target1 = os.path.join(path, k3)
-            save_path = os.path.join(save_path, k3)
-            make_folder(save_path)
+            save_path2 = os.path.join(save_path1, k3)
+            make_folder(save_path2)
             if not check_kind(target1):
                 record_err_log(err_log, target1+' is not found.')
                 continue
             for dk in os.listdir(target1):
                 target2 = os.path.join(target1, dk)
-                save_path2 = os.path.join(save_path, dk)
-                make_folder(save_path2)
+                save_path3 = os.path.join(save_path2, dk)
+                make_folder(save_path3)
                 if not check_kind(target2):
                     record_err_log(err_log, target2+' is not found.')
                     continue
                 print('Now: '+target2)
-                extract(target2, save_path2)  # extract lips
-                if not check_lips_length(target2, save_path2):  # check length of result.
-                    record_err_log(err_log, save_path2+' is not equal frame length.')
-                write_path(path_file, save_path2)
+                extract(target2, save_path3)  # extract lips
+                if not check_lips_length(target2, save_path3):  # check length of result.
+                    record_err_log(err_log, save_path3+' is not equal frame length.')
+                write_path(path_file, save_path3)
 main()
